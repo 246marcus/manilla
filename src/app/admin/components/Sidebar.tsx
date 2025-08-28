69400b4fba06c1b4ef7b48d25e5f49c3afaa8a16
MARCUS <adewaleibukun246@gmail.com>
2025-08-28 15:19:29 +0100

    fixed the navigation in the SiderBar

--- src/app/admin/components/Sidebar.tsx
+++ src/app/admin/components/Sidebar.tsx
@@ -1,6 +1,6 @@
 "use client";
-import sidebarimg from "../../../../public/images/sidebarimage.png"
-import logo from "../../../../public/images/dashlogo.png"
+import sidebarimg from "../../../../public/images/sidebarimage.png";
+import logo from "../../../../public/images/dashlogo.png";
 
 import { Sidebar, Menu, MenuItem } from "react-pro-sidebar";
 
@@ -12,13 +12,13 @@ import {
   FaNewspaper,
   FaSignOutAlt,
 } from "react-icons/fa";
-
+import Link from "next/link";
 
 export default function AppSidebar() {
   return (
     <div className="h-screen relative ">
       <Sidebar
-      image={sidebarimg.src}
+        image={sidebarimg.src}
         //backgroundColor="#1f2937" // Tailwind slate-800
         rootStyles={{
           color: "white",
@@ -26,40 +26,53 @@ export default function AppSidebar() {
           /*  paddingTop: "120px", */
         }}
       >
-      
-        <img alt="logo" src={logo.src} className="mx-auto my-6 w-auto"/>
+        <img alt="logo" src={logo.src} className="mx-auto my-6 w-auto" />
 
         <Menu className="px-4 ">
-          <MenuItem
-            icon={<FaThLarge />}
-            className=" text-white bg-black/60 rounded-md mx-auto border-white/30 border hover:bg-white/90 hover:text-black/80 my-1 "
-          >
-            Dashboard
-          </MenuItem>
-          <MenuItem
-            icon={<FaBlog />}
-            className=" text-white bg-black/60 rounded-md mx-auto border-white/30 border hover:bg-white/90 hover:text-black/80 my-1 "
-          >
-            Blog Management
-          </MenuItem>
-          <MenuItem
-            icon={<FaEnvelope />}
-            className=" text-white bg-black/60 rounded-md mx-auto border-white/30 border hover:bg-white/90 hover:text-black/80 my-1 "
-          >
-            Mail Management
-          </MenuItem>
-          <MenuItem
-            icon={<FaList />}
-            className=" text-white bg-black/60 rounded-md mx-auto border-white/30 border hover:bg-white/90 hover:text-black/80 my-1 "
-          >
-            Waitlist
-          </MenuItem>
-          <MenuItem
-            icon={<FaNewspaper />}
-            className=" text-white bg-black/60 rounded-md mx-auto border-white/30 border hover:bg-white/90 hover:text-black/80 my-1 "
-          >
-            Newsletters
-          </MenuItem>
+          <Link href="/admin/Dashboard">
+            <MenuItem
+              icon={<FaThLarge />}
+              className="text-white bg-black/60 rounded-md mx-auto border-white/30 border hover:bg-white/90 hover:text-black/80 my-1"
+            >
+              Dashboard
+            </MenuItem>
+          </Link>
+
+          <Link href="/admin/blogManagement">
+            <MenuItem
+              icon={<FaBlog />}
+              className="text-white bg-black/60 rounded-md mx-auto border-white/30 border hover:bg-white/90 hover:text-black/80 my-1"
+            >
+              Blog Management
+            </MenuItem>
+          </Link>
+
+          <Link href="/admin/mail">
+            <MenuItem
+              icon={<FaEnvelope />}
+              className="text-white bg-black/60 rounded-md mx-auto border-white/30 border hover:bg-white/90 hover:text-black/80 my-1"
+            >
+              Mail Management
+            </MenuItem>
+          </Link>
+
+          <Link href="/admin/waitlist">
+            <MenuItem
+              icon={<FaList />}
+              className="text-white bg-black/60 rounded-md mx-auto border-white/30 border hover:bg-white/90 hover:text-black/80 my-1"
+            >
+              Waitlist
+            </MenuItem>
+          </Link>
+
+          <Link href="/admin/newsletters">
+            <MenuItem
+              icon={<FaNewspaper />}
+              className="text-white bg-black/60 rounded-md mx-auto border-white/30 border hover:bg-white/90 hover:text-black/80 my-1"
+            >
+              Newsletters
+            </MenuItem>
+          </Link>
         </Menu>
 
         {/* Logout button pinned bottom */}
--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,13 +1,13 @@
-import Dashboard from "./Dashboard/page";
+// import Dashboard from "./Dashboard/page";
 
-export default function Home() {
-  return (
-    <div className="flex bg-gray-100">
-      {/* Main Content */}
-      <div className="flex-1 h-screen overflow-y-auto">
-        <Dashboard />
-        {/* <BlogManagement /> */}
-      </div>
-    </div>
-  );
-}
+// export default function Home() {
+//   return (
+//     <div className="flex bg-gray-100">
+//       {/* Main Content */}
+//       <div className="flex-1 h-screen overflow-y-auto">
+//         {/* <Dashboard /> */}
+//         {/* <BlogManagement /> */}
+//       </div>
+//     </div>
+//   );
+// }
